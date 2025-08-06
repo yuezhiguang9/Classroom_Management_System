498112bb64aeff4f1e96b540689854be4c979f40
chiara261 <3212931856@qq.com>
2025-08-07 00:21:23 +0800

    删除了错误的上传结构
    删除了嵌套结构

--- Campus_Management_System/Campus_Management_System/src/main/java/demo/campus_management_system/dao/dao_interface/UsersMapper.java
+++
@@ -1,7 +0,0 @@
-package demo.campus_management_system.dao.dao_interface;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import demo.campus_management_system.entity.Users;
-
-public interface UsersMapper extends BaseMapper<Users> {
-}
